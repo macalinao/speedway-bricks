e23a81a740977b8b81fb46fa6a155f1cccbddbcf
Ian Macalinao <me@ian.pw>
2018-01-14 08:27:45 -0600

    fix contstructor name

--- contracts/SpeedwayBricks.sol
+++ contracts/SpeedwayBricks.sol
@@ -13,7 +13,7 @@ contract SpeedwayBricks is StandardToken {
   /**
    * @dev Constructor that gives msg.sender all of existing tokens.
    */
-  function SimpleToken() public {
+  function SpeedwayBricks() public {
     totalSupply = INITIAL_SUPPLY;
     balances[msg.sender] = INITIAL_SUPPLY;
     Transfer(0x0, msg.sender, INITIAL_SUPPLY);

91dc6d2d83477e3080624dae9df110fcfb5ddaab
Ian Macalinao <me@ian.pw>
2018-01-14 08:28:12 -0600

    1 billion bricks

--- contracts/SpeedwayBricks.sol
+++ contracts/SpeedwayBricks.sol
@@ -8,7 +8,7 @@ contract SpeedwayBricks is StandardToken {
   string public constant symbol = "BRKS";
   uint8 public constant decimals = 18;
 
-  uint256 public constant INITIAL_SUPPLY = 10000 * (10 ** uint256(decimals));
+  uint256 public constant INITIAL_SUPPLY = 1000000000 * (10 ** uint256(decimals));
 
   /**
    * @dev Constructor that gives msg.sender all of existing tokens.
